310cf2ac62ea0fd4d49034e8f0e236a7f8353243
krlmari <krylova_m_v@inbox.ru>
2024-01-10 22:08:10 +0100

    add tailwind example

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+            <div className="mx-auto my-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-12 lg:max-w-7xl lg:grid-cols-2 lg:px-8 h-full">
+                <div>
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Hello world!</h2>
+                    <p className="mt-4 text-gray-500">
+                        Dictum. Integer lectus mattis nisi leo, cursus malesuada dictum. Amet venenatis pulvinar sed consectetur et risus
+                        libero, vitae non vestibulum faucibus. Mattis amet lacinia amet vel molestie dictumst.
+                    </p>
+                </div>
+                <div>
+                    <img
+                        src="https://images.unsplash.com/photo-1702560030602-1b780380b892?q=80&w=2999&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        alt="Card"
+                        className="rounded-lg"
+                    />
+                </div>
+            </div>
         </div>
     );
 }
